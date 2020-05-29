f4040bd39998f1b50469e498edb82717f87ca2d9
Sokhibjon Orzikulov <genemators@gmail.com>
2020-05-29 23:40:36 +0500

    Update start.py

--- commands/start.py
+++ commands/start.py
@@ -6,7 +6,7 @@ def _start():
     def __start(message):
         text = \
             "<b>Bizning botimizga xush kelibsiz!</b> \n" \
-            f"<i>Hurmatli {message.message_from.first_name}, sizni ko'rib turganimizdan mamnunmiz!"
+            f"<i>Hurmatli {message.from_user.first_name}, sizni ko'rib turganimizdan mamnunmiz!</i>"
         bot.reply_to(message, text, parse_mode="HTML")
         print(message)
     pass
